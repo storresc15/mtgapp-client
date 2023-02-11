09289054b9408c1eabfd5076656f808a545d0bfa
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-02-10 23:18:16 -0600

    Fixed ecma version

--- src/components/SingleDeckDisplay.js
+++ src/components/SingleDeckDisplay.js
@@ -32,7 +32,6 @@ const SingleDeckDisplay = ({
             <img
               src="https://media.magic.wizards.com/image_legacy_migration/magic/images/mtgcom/fcpics/making/mr224_back.jpg"
               alt={deckId}
-              heigth="150"
               width="150"
             ></img>
           </Link>
